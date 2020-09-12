bad54fbdd43d192d2c4ebada4c06e3d3d48eb988
Ebbie <ebenezer.arapally@gmail.com>
2020-09-12 01:23:42 -0400

    git rm -r --cached .

--- Python Basics/basics1.py
+++
@@ -1,9 +0,0 @@
-import datetime
-mynow = datetime.datetime.now()
-print(mynow)
-
-
-mynumber = 10
-mytext = "Hello"
-
-print(mynumber, mytext)
--- Python Basics/basics4.py
+++
@@ -1,8 +0,0 @@
-#Dictionary
-student_grades = {"Mary": 9.1, "Sim": 8.8, "John": 7.5}
-print(student_grades)
-mysum = sum(student_grades.values())
-
-#Tuples
-monday_temperatures = (1,4,5)
-print(monday_temperatures)
