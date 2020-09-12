63c4ab1e2bf2942f0318ca94570a16d214d3633f
Ebbie <ebenezer.arapally@gmail.com>
2020-09-12 01:28:46 -0400

    Add test.py

--- Python Basics/basics4.py
+++ Python Basics/basics4.py
@@ -3,6 +3,6 @@ student_grades = {"Mary": 9.1, "Sim": 8.8, "John": 7.5}
 print(student_grades)
 mysum = sum(student_grades.values())
 
-#Tuples
+#Tuples are immutable
 monday_temperatures = (1,4,5)
 print(monday_temperatures)
