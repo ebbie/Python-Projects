2fb2240214e2c2666c50965eda459e3518ff666f
Ebbie <ebenezer.arapally@gmail.com>
2020-09-12 00:37:55 -0400

    Add Python Basics program

---
+++ Python Basics/basics4.py
@@ -0,0 +1,8 @@
+#Dictionary
+student_grades = {"Mary": 9.1, "Sim": 8.8, "John": 7.5}
+print(student_grades)
+mysum = sum(student_grades.values())
+
+#Tuples
+monday_temperatures = (1,4,5)
+print(monday_temperatures)
